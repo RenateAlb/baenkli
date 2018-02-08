c536b3a86ca85ac9f12157eb7fedfd65d4325f02
Fernando González Cortés <fernando.gonzalez@geomati.co>
2018-02-08 08:28:19 +0100

    stats WIP

--- stats.js
+++ stats.js
@@ -5,13 +5,19 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var table = document.getElementById('stats');
     var stats = JSON.parse(xhr.responseText);
-    for (var i = 0; i < stats.length; i++) {
+    for (var i = 0; i < stats.length / 2; i++) {
         var row = table.insertRow();
         var cell = row.insertCell();
         var textNode = document.createTextNode(stats[i].name);
         cell.appendChild(textNode);
         cell = row.insertCell();
         textNode = document.createTextNode(stats[i].count);
+        cell.appendChild(textNode);
+        cell = row.insertCell();
+        textNode = document.createTextNode(stats[i+stats.length/2].name);
+        cell.appendChild(textNode);
+        cell = row.insertCell();
+        textNode = document.createTextNode(stats[i+stats.length/2].count);
         cell.appendChild(textNode);
     }
   }
